ccaa787611dea36106b6af4ded46bb4a48dc4255
Alexsand74 <1974alexsand@gmail.com>
2023-07-21 13:17:50 +0300

    homework 2.5. Collections: sheets and sets, is here!

---
+++ src/main/java/my/sky/pro/homework2_5/exception/EmployeeStorageIsFullException.java
@@ -0,0 +1,8 @@
+package my.sky.pro.homework2_5.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR) // внутренняя ошибка сервера - ошибка номер 500
+public class EmployeeStorageIsFullException extends RuntimeException{ //наследуемся от непроверяемого исключения
+}
